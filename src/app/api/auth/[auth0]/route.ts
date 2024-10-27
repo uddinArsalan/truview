a7d392da557e81ea2dd039f02d5daf71f1a69f02
Arsalan <uddinarsalan91@gmail.com>
2024-10-28 01:27:15 +0530

    update auth0 configurations

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,16 +1,21 @@
-import { handleAuth,handleLogin,handleLogout } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin, handleLogout } from "@auth0/nextjs-auth0";
+const baseUrl = process.env.AUTH0_BASE_URL;
 
 export const GET = handleAuth({
   login: handleLogin({
+    authorizationParams: {
+      redirect_uri: `${baseUrl}/api/auth/callback`,
+    },
     returnTo: "/",
   }),
   signup: handleLogin({
     authorizationParams: {
       screen_hint: "signup",
+      redirect_uri: `${baseUrl}/api/auth/callback`,
     },
     returnTo: "/",
   }),
-  logout : handleLogout({
-    returnTo : "/sigIn"
-  })
+  logout: handleLogout({
+    returnTo: `${baseUrl}`,
+  }),
 });
