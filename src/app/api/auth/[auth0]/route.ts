26e60448c8eef4d8801403a620e84ed035f16931
Arsalan <uddinarsalan91@gmail.com>
2024-10-29 21:14:06 +0530

    tried to remove some problematic code

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,32 +1,14 @@
 import { handleAuth, handleLogin, handleLogout } from "@auth0/nextjs-auth0";
-const baseUrl = process.env.AUTH0_BASE_URL || 'http://localhost:3000';
+const baseUrl = process.env.AUTH0_BASE_URL || "http://localhost:3000";
 
 export const GET = handleAuth({
   login: handleLogin({
-    authorizationParams: {
-      redirect_uri: `${baseUrl}/api/auth/callback`,
-      response_type: 'code',
-      scope: 'openid profile email',
-    },
     returnTo: "/",
   }),
   signup: handleLogin({
     authorizationParams: {
       screen_hint: "signup",
-      redirect_uri: `${baseUrl}/api/auth/callback`,
-      response_type: 'code',
-      scope: 'openid profile email',
     },
     returnTo: "/",
   }),
-  logout: handleLogout({
-    returnTo: `${baseUrl}`,
-  }),
 });
-
-export const config = {
-  api: {
-    externalResolver: true,
-    bodyParser: false,
-  },
-};
